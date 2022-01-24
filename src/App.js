ec011527db63d82a65d073c43acad4750089ff31
Alexander Nagy <nagyal3x@gmail.com>
2022-01-24 23:36:45 +0100

    mui not

--- src/App.js
+++ src/App.js
@@ -1,56 +1,20 @@
 import React, { useState } from "react";
 import "./App.css";
-// import { useCharacters, useLocations } from "./api/useData";
 import Logo from "./components/Logo";
 import CharactersButton from "./components/CharactersButton";
 import LocationsButton from "./components/LocationsButton";
-import Description from "./components/Description";
-import ListOfCharacters from "./components/ListOfCharacters";
-import ListOfLocations from "./components/ListOfLocations";
+
 
 
 function App() {
-  // const characters = useCharacters(1);
-  // const locations = useLocations(1);
-
-  // console.log("Characters data: ");
-  // console.log(characters);
-  // console.log("Locations data: ");
-  // console.log(locations);
-
-  // const [showCharList, setShowCharList] = useState(false);
-  // const [showLocList, setShowLocList] = useState(false);
-  const [sectionName, setSectionName] = useState("Description");
-
-  // const CharactersClickEvent = () => {
-  //   setShowCharList(true);
-  //   setShowLocList(false);
-  // };
-
-  // const LocationsClickEvent = () => {
-  //   setShowLocList(true);
-  //   setShowCharList(false)
-  // };
-
-
   return (
-  <div className="App">
-    <Logo/>
-    <div className="buttonsDiv">
-      {/* <CharactersButton onClick={CharactersClickEvent}/>
-      <LocationsButton onClick={LocationsClickEvent}/> */}
-      <button onClick={() => setSectionName ("CharList")}>Characters</button>
-      <button onClick={() => setSectionName ("LocList")}>Locations</button>
-
+    <div className="App">
+      <Logo />
+      <div className="buttonsDiv">
+        <CharactersButton />
+        <LocationsButton />
+      </div>
     </div>
-    <div>
-      {sectionName === "Description" && <Description/>}
-      {sectionName === "CharList" && <ListOfCharacters/>}
-      {sectionName === "LocList" && <ListOfLocations/>}
-      {/* <ListOfCharacters {...showCharList && showLocList ? <Description/> : <ListOfCharacters/>}/>
-      <ListOfLocations {...showCharList && showLocList ? <Description/> : <ListOfLocations/>}/> */}
-    </div>
-  </div>
   );
 }
 
